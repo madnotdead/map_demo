bf1ba4a59c57a3ab38f68269d33d07622bad1cca
Leandro Maguna <leandromaguna@gmail.com>
2016-06-10 17:45:12 -0300

    Added method to get current location

--- app/src/main/java/com/example/leandromaguna/myapp/DemoMapActivity.java
+++ app/src/main/java/com/example/leandromaguna/myapp/DemoMapActivity.java
@@ -2,9 +2,15 @@ package com.example.leandromaguna.myapp;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,9 +21,14 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by O23002 on 23/05/2016.
  */
-public class DemoMapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class DemoMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     private GoogleMap mMap;
+    private GoogleApiClient mApiClient;
+
+    private Location currentLocation;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,31 @@ public class DemoMapActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        buildGoogleApiClient();
+    }
+
+    private void buildGoogleApiClient(){
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mApiClient.isConnected())
+            mApiClient.disconnect();
     }
 
     @Override
@@ -33,5 +69,26 @@ public class DemoMapActivity extends FragmentActivity implements OnMapReadyCallb
         mMap = googleMap;
 
         mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+
+        if(currentLocation != null){
+            Log.w("CurrentLocation", "Latitud: " + currentLocation.getLatitude() + " - " + "Latitud: " +currentLocation.getLongitude());
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this,"Connection failed: " + connectionResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
     }
 }
